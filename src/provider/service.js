e9a7872f6cdcf8faba3181a08a227b34b7df17dc
hitmanche <52356400+hitmanche@users.noreply.github.com>
2020-02-03 09:48:18 +0300

    service updated

--- src/provider/service.js
+++ src/provider/service.js
@@ -5,7 +5,6 @@ export function FetchGet(apiLink) {
     return new Promise((resolve, reject) => {
         fetch(baseLink + apiLink, {
             method: 'get', headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)'
             }
         }).then(response => {
             if (response.status !== 201 && response.status !== 202 && response.status !== 200) {
